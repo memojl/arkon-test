e560375accd59f64d20e2cbcccdff9822e2fd589
memojl <memojl08@gmail.com>
2023-03-24 17:45:00 -0600

    Update index

--- wp-content/themes/arkontheme/index.php
+++ wp-content/themes/arkontheme/index.php
@@ -88,5 +88,139 @@
         </div>
     </div>
 </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 bg">
+            <div class="logo-container">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo-arkon-data.png" class="bg-logo" alt="">
+            </div>
+            <div class="bg-title">
+                <div class="bg-title-box">
+                    <span class="bg-title-box-master">
+                        Master control
+                    </span>
+                    <span class="bg-title-box-span">
+                        <span>&</span>harmonize data
+                    </span>
+                    <span class="bg-title-box-get">
+                        <a href="#" class="bg-title-btn">Get a demo</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 bg2">
+            <div class="row">
+                <div class="col-lg-7">&nbsp;</div>
+                <div class="col-lg-3">
+                    <div class="bg2-copy1">
+                        Are you really in control
+                    </div>
+                    <div class="bg2-copy2">
+                        of your data?
+                    </div>
+                    <div class="bg2-copy3">
+                        Keytar McSweeney's Williamsburg, readymade leggings try-hard 90's street art letterpress hoodie occupy Wes Anderson Banksy. Asymmetrical viral letterpress, McSweeney's seitan 3 wolf moon drinking vinegar sartorial pour-over messenger bag photo booth umami next level craft beer. Tote bag 8-bit flannel dreamcatcher photo lo-fi vegan pop-up chia small batch DIY seitan asymmetrical viral. Disrupt salvia bespoke craft beer master cleanse. Butcher cornhole gentrify pop-up, raw denim viral literally.
+                    </div>
+                    <div>
+                        <a href="#" class="bg2-btn">Get a demo</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row bg-mt-30">
+        <div class="col-lg-12 container-bg3">
+            <div class="figure1"></div>
+            <div class="col-lg-7">&nbsp;</div>
+            <div class="col-lg-5 offset-lg-7">
+                <div class="bg3-copy1">
+                    Our
+                </div>
+                <div class="bg3-copy2">
+                    solution
+                </div>
 
+                <div class="bg3-items">
+                    <div class="bg3-items-card">
+                        <div class="bg3-items-ima">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/folder.png" width="105" alt="" />
+                        </div>
+                        <div class="bg3-items-title-1 pt-4 pb-2">
+                            Governance
+                        </div>
+                        <div class="bg3-items-copy">
+                            Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. 
+                        </div>
+                    </div>
+                    <div class="bg3-items-card">
+                        <div class="bg3-items-ima">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/lupa.png" width="105" alt="" />
+                        </div>
+                        <div class="bg3-items-title-2 pt-4 pb-2">
+                            DATAOPS
+                        </div>
+                        <div class="bg3-items-copy">
+                            Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis . 
+                        </div>
+                    </div>
+                    <div class="bg3-items-card">
+                        <div class="bg3-items-ima">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/analytic.png" width="105" alt="" />
+                        </div>
+                        <div class="bg3-items-title-3 pt-4 pb-2">
+                            Visualization
+                        </div>
+                        <div class="bg3-items-copy">
+                            Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. 
+                        </div>
+                    </div>
+                    <div class="bg3-items-card">
+                        <div class="bg3-items-ima">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/gear.png" width="105" alt="" />
+                        </div>
+                        <div class="bg3-items-title-4 pt-4 pb-2">
+                            PREPARATION
+                        </div>
+                        <div class="bg3-items-copy">
+                            Vestibulum rutrum quam vitae fringilla tincidunt. Suspendisse nec tortor urna. Ut laoreet sodales nisi, quis iaculis nulla iaculis.
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            <div class="bg3"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 container-bg4">
+            <div class="figure2"></div>
+            <div class="col-lg-4 offset-lg-1">
+                <div class="bg4-copy1">
+                    Is your analysis based on updated,
+                </div>
+                <div class="bg4-copy2">
+                    trustworthy data? 
+                </div>
+                <div class="bg4-copy3">
+                    Keytar McSweeney's Williamsburg, readymade leggings try-hard 90's street art letterpress hoodie occupy Wes Anderson Banksy. Asymmetrical viral letterpress, McSweeney's seitan 3 wolf moon drinking vinegar sartorial pour-over messenger bag photo booth umami next level craft beer. Tote bag 8-bit flannel dreamcatcher photo lo-fi vegan pop-up chia small batch DIY seitan asymmetrical viral. Disrupt salvia bespoke craft beer master cleanse. Butcher cornhole gentrify pop-up, raw denim viral literally.
+                </div>
+                <div>
+                    <a href="#" class="bg4-btn">Discover more</a>
+                </div>
+            </div>
+            <div class="bg4"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 container-bg5">
+            <div class="bg5"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 container-laptop">
+            <div class="laptop"></div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
