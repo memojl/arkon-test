1acdd25cefdcb0ca2784181f5213ad3e96e2d5f5
memojl <memojl08@gmail.com>
2023-03-25 09:37:55 -0600

    Update Footer

--- wp-content/themes/arkontheme/footer.php
+++ wp-content/themes/arkontheme/footer.php
@@ -1,6 +1,6 @@
 <?php wp_footer(); ?>
     <footer>
-    All Righs Reserver @ 2019 Arkon Data  |  More information:  info@arkondata.com
+        All Righs Reserver @ 2019 Arkon Data  |  More information: <a href="mailto:info@arkondata.com">info@arkondata.com</a>
     </footer>
 </body>
 </html>
