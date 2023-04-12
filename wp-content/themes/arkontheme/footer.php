a5aaf42c0adcc463541a3cfd8733184ac8513c9b
memojl <memojl08@gmail.com>
2023-04-12 16:10:26 -0600

    Update footer

--- wp-content/themes/arkontheme/footer.php
+++ wp-content/themes/arkontheme/footer.php
@@ -2,20 +2,5 @@
         All Righs Reserver @ 2019 Arkon Data  |  More information: &nbsp;<a href="mailto:info@arkondata.com">info@arkondata.com</a>
     </footer>
     <?php wp_footer(); ?>
-<script src='https://apirestm.000webhostapp.com/api/email/test/index.js' type='text/javascript'></script>
-<!--
-<script>
-    function loadJS(url) {
-        let script = document.createElement('script');
-        script.src = url;
-        document.body.appendChild(script);
-    }
-    const {origin, host} = window.location;
-    console.log(host, origin);
-    const url = (host == 'localhost')?origin+'/MisSitios/apirestm/api/email/test/index.js':'https://apirestm.000webhostapp.com/api/email/test/index.js';
-    console.log(url);
-    loadJS(url);
-</script>
--->
 </body>
 </html>
