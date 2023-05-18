ec00e47d794b6baf69de01e806e56a9c2c54a5ec
memojl <memojl08@gmail.com>
2023-05-18 07:12:00 -0600

    Update assets

--- wp-content/themes/arkontheme/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer>
-        All Righs Reserver @ 2019 Arkon Data  |  More information: &nbsp;<a href="mailto:info@arkondata.com">info@arkondata.com</a>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
--- wp-content/themes/arkontheme/js/apiApp.js
+++
@@ -1,9 +0,0 @@
-function loadJS(url) {
-    let script = document.createElement('script');
-    script.src = url;
-    document.body.appendChild(script);
-}
-const {origin, host} = window.location; console.log(host, origin);
-const apiUrl = (host == 'localhost')?origin+'/MisSitios/apirestm/api/':'https://apirestm.000webhostapp.com/api/';
-console.log('apiUrl',apiUrl);
-loadJS(apiUrl+'email/test/index.js');
